1f5fbfaa830d529f1f3b6e6dffeb26ed8e3b6e75
Rishab Prabhu <rishab@Rishabs-MacBook-Pro.local>
2026-03-17 12:17:15 -0500

    initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <h1 className="title">Madtown Mayhem</h1>
+
+      <button 
+        className="ticketButton"
+        onClick={() => setCount(count + 1)}
+      >
+        Tickets Sold: {count}
+      </button>
+
     </div>
   );
 }
